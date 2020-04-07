a132e1bfe49f609a3b64f0c7f385fb643b94dc75
Joel Thomas <hello@joelcthomas.com>
2020-04-07 01:20:38 -0400

    Adding python files

---
+++ config/init.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# Config for AzureML Workspace
+# for secure keeping, store credentials in Azure Key Vault and link using Azure Databricks secrets with dbutils
+subscription_id = "" 
+resource_group = ""                       
+workspace_name = ""                         
+
+tenant_id = ""
+sp_id = ""
+sp_secret = ""
